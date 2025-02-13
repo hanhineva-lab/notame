f8bf3ce7162b597f5ea8a046916cbcc14317e1e9
vsuksi <vsuksi@abo.fi>
2025-02-13 14:13:15 +0200

    R CMD check NOTEs: Change startup message to use packageStartupMessage() in .onAttach
    Changed this for a cleaner submission although message may be suppressed 
    by suppressStartupMessages and is only displayed when attached 
    (library() or require()), not when loaded for another package.

--- R/utils.R
+++ R/utils.R
@@ -180,16 +180,18 @@ NULL
 
 utils::globalVariables(c('i', '.'))
 
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage("NOTE This is a development version. There is active 
+  development with breaking changes until the package has been approved in 
+  Bioconductor. The original notame package this development is based on can be 
+  installed using `devtools::install_github('antonvsdata/notame@v0.3.1')`")
+}
+
 #' Set default color scales on load
 #'
 #' @param libname,pkgname default parameters
 #' @noRd
 .onLoad <- function(libname, pkgname) {
-  message("NOTE This is a development version. There is active development with 
-  breaking changes until the package has been approved in Bioconductor. Yet, 
-  everything in the main branch is to our knowledge working as it should. The 
-  original notame package this development is based on can be installed using 
-  `devtools::install_github('antonvsdata/notame@v0.3.1')`")
   op <- options()
   op_notame <- list(
     notame.citations = list(
